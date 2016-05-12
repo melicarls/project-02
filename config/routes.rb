1eb8f4bad1dd68d4cc3f8cf5de0bb3cbe7712d98
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-05-12 15:12:38 -0700

    fixed small route issue

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
   #Recipe Routes
-    get '/recipes/new', to: 'recipes#new', as: new_recipe
+    get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
     post '/recipes', to: 'recipes#create'
-    
+
 end
