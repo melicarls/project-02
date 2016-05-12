8165c50a4fe45c099a9fe4261598c6c45c29d9de
melicarls <melissacarlson15@gmail.com>
2016-05-12 14:33:37 -0700

    ran migration to add ingredient to schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160512212938) do
+ActiveRecord::Schema.define(version: 20160512213151) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredients", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "recipes", force: :cascade do |t|
     t.string   "title"
