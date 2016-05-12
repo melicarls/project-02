d38af8141b5490b4704ee5d094ed46fc3789f76a
Franklin Zuniga <Franklin@slyle-mac15.local>
2016-05-12 15:39:07 -0700

    made routes to edit and update

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,8 @@ Rails.application.routes.draw do
   #Recipe Routes
     get '/recipes/new', to: 'recipes#new', as: 'new_recipe'
     post '/recipes', to: 'recipes#create'
+    get '/recipes/:id/edit', to: 'recipes#edit', as: 'edit_recipe'
+    patch 'recipes/:id', to: 'recipes#update'
+
 
 end
