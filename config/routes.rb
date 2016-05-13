45d6d17dcb9f4f35565f27db8aa8697fd6c3ed16
melicarls <melissacarlson15@gmail.com>
2016-05-13 14:15:40 -0700

    reordered routes

--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,12 @@ Rails.application.routes.draw do
   patch '/recipes/:id', to: 'recipes#update'
   delete '/recipes/:id', to: 'recipes#destroy', as: 'destroy_recipe'
 
+  devise_for :users
 
   get '/users/:id', to: 'users#show', as: 'user'
 
 
 
-  devise_for :users
 
 
 
