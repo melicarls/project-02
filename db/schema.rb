043311baf458bc96b565ffd5c9cd8c7972b22da6
justincastilla <pxlperfection@gmail.com>
2016-05-13 14:14:53 -0700

    added pepper to meatloaf

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,26 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160513172644) do
+ActiveRecord::Schema.define(version: 20160513210729) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "ingredient_recipes", force: :cascade do |t|
+    t.integer  "ingredient_id"
+    t.integer  "recipe_id"
+    t.string   "quantity"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  add_index "ingredient_recipes", ["ingredient_id"], name: "index_ingredient_recipes_on_ingredient_id", using: :btree
+  add_index "ingredient_recipes", ["recipe_id"], name: "index_ingredient_recipes_on_recipe_id", using: :btree
 
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-  end
-
-  create_table "ingredients_recipes", id: false, force: :cascade do |t|
-    t.integer "ingredient_id", null: false
-    t.integer "recipe_id",     null: false
-    t.string  "quantity"
   end
 
   create_table "recipes", force: :cascade do |t|
@@ -38,4 +43,6 @@ ActiveRecord::Schema.define(version: 20160513172644) do
     t.string   "missing_ingredients", default: [],              array: true
   end
 
+  add_foreign_key "ingredient_recipes", "ingredients"
+  add_foreign_key "ingredient_recipes", "recipes"
 end
