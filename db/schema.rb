24a749532e680245746f6b1b5a68ac757f75b5e1
lsquach <lsquach@gmail.com>
2016-05-13 14:34:24 -0700

    saved schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,8 +16,6 @@ ActiveRecord::Schema.define(version: 20160513210729) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-<<<<<<< HEAD
-=======
   create_table "ingredient_recipes", force: :cascade do |t|
     t.integer  "ingredient_id"
     t.integer  "recipe_id"
@@ -29,7 +27,6 @@ ActiveRecord::Schema.define(version: 20160513210729) do
   add_index "ingredient_recipes", ["ingredient_id"], name: "index_ingredient_recipes_on_ingredient_id", using: :btree
   add_index "ingredient_recipes", ["recipe_id"], name: "index_ingredient_recipes_on_recipe_id", using: :btree
 
->>>>>>> development
   create_table "ingredients", force: :cascade do |t|
     t.string   "name"
     t.datetime "created_at", null: false
