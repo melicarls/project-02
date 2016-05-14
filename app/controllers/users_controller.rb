5b1fd503bf72c96e8dddf48ed801abe160ab165e
lsquach <lsquach@gmail.com>
2016-05-14 14:57:51 -0700

    removed recipes section from user show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-    @recipe = Recipe.all
   end
-  private
-  def recipe_params
-    params.require(:recipe).permit(:title, :author, :image, :directions)
-  end
+  
 end
