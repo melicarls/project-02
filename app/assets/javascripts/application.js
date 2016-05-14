c2c3231564120e3cdee7e6b8885fb4b2af105ed5
melicarls <melissacarlson15@gmail.com>
2016-05-14 15:10:25 -0700

    refactored search javascript

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,25 +15,17 @@
 //= require turbolinks
 //= require_tree .
 jQuery(document).ready(function($){
-
-$('.live-search-list li').each(function(){
-$(this).attr('data-search-term', $(this).text().toLowerCase());
-});
-
-$('.live-search-box').on('keyup', function(){
-
-var searchTerm = $(this).val().toLowerCase();
-
+  $('.live-search-list li').each(function(){
+    $(this).attr('data-search-term', $(this).text().toLowerCase());
+  });
+  $('.live-search-box').on('keyup', function(){
+  var searchTerm = $(this).val().toLowerCase();
     $('.live-search-list li').each(function(){
-
-        if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
-            $(this).show();
-        } else {
-            $(this).hide();
-        }
-
+      if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
+          $(this).show();
+      } else {
+          $(this).hide();
+      }
     });
-
-});
-
+  });
 });
