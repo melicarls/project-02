8635fc9db03dd565ec62ee4691ce8eb6ab727b40
melicarls <melissacarlson15@gmail.com>
2016-05-14 11:43:07 -0700

    ingredient searched defaults to false

--- db/migrate/20160514183847_add_searched_to_ingredients.rb
+++ db/migrate/20160514183847_add_searched_to_ingredients.rb
@@ -1,5 +1,5 @@
 class AddSearchedToIngredients < ActiveRecord::Migration
   def change
-    add_column :ingredients, :searched, :boolean
+    add_column :ingredients, :searched, :boolean, :default => false
   end
 end
