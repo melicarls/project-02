7976a341437c84e828abc3ad31683abba776b461
lsquach <lsquach@gmail.com>
2016-05-16 16:20:39 -0700

    updated default image to check for blank? instead of nil?. updated edit profile redirect to user show page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,11 +6,10 @@ class ApplicationController < ActionController::Base
   include RecipesHelper
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
-protected
-
-    def configure_permitted_parameters
-        devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password) }
-        devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password, :image) }
-    end
+  protected
+  def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password) }
+      devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:first_name, :last_name, :email, :password, :current_password, :image) }
+  end
 
 end
