d8cbac23e0daa1f99108fd325429c0b16c7940e7
melicarls <melissacarlson15@gmail.com>
2016-05-16 17:56:15 -0700

    wrapped ingredients in buttony divs

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,24 +18,18 @@
 $(document).on('ready', function(){
 
   // Filter search
-  $('.live-search-list li').each(function(){
+  $('.live-search-list .ck-button').each(function(){
     $(this).attr('data-search-term', $(this).text().toLowerCase());
   });
   $('.live-search-box').on('keyup', function(){
   var searchTerm = $(this).val().toLowerCase();
-    $('.live-search-list li').each(function(){
+    $('.live-search-list .ck-button').each(function(){
       if ($(this).filter('[data-search-term *= ' + searchTerm + ']').length > 0 || searchTerm.length < 1) {
           $(this).show();
       } else {
           $(this).hide();
       }
     });
-  });
-
-
-  // Select ingredients with buttons
-  $('.ingredient').on('click', function() {
-    $(this).preventDefault();
   });
 
 });
