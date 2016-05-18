99c4209318ca29fb296f9dd12599ff5ac2bc1495
melicarls <melissacarlson15@gmail.com>
2016-05-17 17:18:07 -0700

    added controller action to remove a recipe from a user's favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,12 +15,25 @@ class UsersController < ApplicationController
       @user.recipes << @recipe
       flash[:success] = "This recipe has been added to your favorites."
     else
-      flash[:error] = "This recipe is already in your favorites."
+      flash[:warning] = "This recipe is already in your favorites."
     end
     redirect_to recipe_path(@recipe)
   end
 
   def removestar
+    @recipe = Recipe.find(params[:id])
+    if !current_user
+      flash[:error] = "Oh no! You're not logged in, so we can't remove anything from your favorites list."
+      redirect_to recipe_path(@recipe)
+    end
+    @user = current_user
+    if @user.recipes.include?(@recipe)
+      @user.recipes.delete(@recipe)
+      flash[:success] = "This recipe has been removed from your favorites"
+    else
+      flash[:warning] = "That recipe isn't in your favorites."
+    end
+    redirect_to recipe_path(@recipe)
   end
 
 end
