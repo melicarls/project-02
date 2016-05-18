f188ab39ba8b9f1e1b0be50809b0da8e5284a735
melicarls <melissacarlson15@gmail.com>
2016-05-17 17:03:03 -0700

    added controller action to store recipes in a user's favorites

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,23 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+  def star
+    @recipe = Recipe.find(params[:id])
+    if !current_user
+      flash[:error] = "Oh no! You're not logged in, so you can't save favorites."
+      redirect_to recipe_path(@recipe)
+    end
+    @user = current_user
+    if !@user.recipes.include?(@recipe)
+      @user.recipes << @recipe
+      flash[:success] = "This recipe has been added to your favorites."
+    else
+      flash[:error] = "This recipe is already in your favorites."
+    end
+    redirect_to recipe_path(@recipe)
+  end
+
+  def removestar
+  end
+
 end
