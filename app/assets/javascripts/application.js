a9162566c8ab7f2a3a1f429c5f8d76af269d4fb2
melicarls <melissacarlson15@gmail.com>
2016-05-19 11:26:17 -0700

    reordered requirements

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require jquery_ujs
+//= require bootstrap-sprockets
 //= require_tree .
 
 $(document).on('ready', function(){
